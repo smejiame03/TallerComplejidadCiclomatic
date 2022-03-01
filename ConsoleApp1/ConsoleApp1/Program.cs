1e4f6718596f0fff406c02f7f55c9abbef7960ef
smejiame03 <100173415+smejiame03@users.noreply.github.com>
2022-02-28 20:14:04 -0500

    Ejercicio 4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número de inscripción");
+            int num_inscripcion = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el nombre del estudiante");
+            String nom_estudiante = Console.ReadLine();
+            Console.WriteLine("Ingrese el patrimonio del estudiante");
+            int patrimonio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el estrato social del estudiante");
+            int estrato_social = int.Parse(Console.ReadLine());
+
+
+            double pagmat = 50000;
+
+
+            if ((patrimonio > 2000000) ^(estrato_social > 3))
+            {
+                pagmat = pagmat + 0.03 * patrimonio;
+            }
+
+            Console.WriteLine("El estudiante con numero de inscripcion:" + num_inscripcion, "y nombre: " + nom_estudiante, "debe pagar $: " + pagmat);
+        }
+    }
+}
